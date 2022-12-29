944aacb28af189a97a0539ffe3fff713b08ef8f1
aumpierre-unb <aumpierre@gmail.com>
2022-12-29 08:42:42 -0300

    Update doplots.jl

--- src/doplots.jl
+++ src/doplots.jl
@@ -15,7 +15,6 @@ function doplot(dots, updown, f, x, y, data, X, q, R)
     xB = X[3]
     xi = X[4]
     yi = X[5]
-
     plot(xlabel="x", ylabel="y",
         xlims=(0, 1), ylims=(0, 1),
         legend=false,
@@ -23,7 +22,6 @@ function doplot(dots, updown, f, x, y, data, X, q, R)
         grid=:true,
         minorgrid=:true,
         size=(500, 400))
-
     if dots
         X = data[:, 1]
         Y = data[:, 2]
@@ -35,40 +33,32 @@ function doplot(dots, updown, f, x, y, data, X, q, R)
         display(plot!(X, Y,
             seriestype=:line, color=:black))
     end
-
     X = [0; 1]
     Y = X
     display(plot!(X, Y,
         seriestype=:line, color=:black,
         linestyle=:dash))
-
     Y = R / (1 + R) .* X .+ xD / (1 + R)
     display(plot!(X, Y,
         seriestype=:line, color=:blue))
-
     display(plot!([xD],
         seriestype=:vline, color=:blue,
         linestyle=:dash))
-
     Y = (xB - yi) / (xB - xi) .* (X .- xi) .+ yi
     display(plot!(X, Y,
         seriestype=:line, color=:red))
-
     display(plot!([xB],
         seriestype=:vline, color=:red,
         linestyle=:dash))
-
     if q != 1 - 1e-10
         Y = q / (q - 1) .* X .- xF / (q - 1)
         display(plot!(X, Y,
             seriestype=:line, color=:magenta,
             linestyle=:dash))
     end
-
     display(plot!([xF],
         seriestype=:vline, color=:magenta,
         linestyle=:dash))
-
     if updown
         display(plot!(x, y,
             seriestype=:steppost, color=:cyan,
