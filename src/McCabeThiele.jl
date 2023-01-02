09b532221ed37a5cc0d1d898facb705c9e49c944
aumpierre-unb <aumpierre@gmail.com>
2023-01-02 13:48:51 -0300

    Update McCabeThiele.jl

--- src/McCabeThiele.jl
+++ src/McCabeThiele.jl
@@ -4,6 +4,12 @@
 the number of theoretical stages of a distillation column 
 using the McCabe-Thiele method.
 
+Author: Alexandre Umpierre `aumpierre@gmail.com`
+
+Maintainer's repository: `https://github.com/aumpierre-unb/McCabe-Thiele.jl`
+
+Citation (any version): `DOI 10.5281/zenodo.7126164`
+
 See also: `stages`, `refmin`, `qR2S`.
 """
 module McCabeThiele
