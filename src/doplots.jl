1fc8d88d746acd26f6ad174b2d91e9d98c4f4bc9
aumpierre-unb <aumpierre@gmail.com>
2023-01-07 07:53:40 -0300

    Update doplots.jl

--- src/doplots.jl
+++ src/doplots.jl
@@ -15,8 +15,10 @@ function doplot(dots, updown, f, x, y, data, X, q, R)
     xB = X[3]
     xi = X[4]
     yi = X[5]
-    plot(xlabel="x", ylabel="y",
-        xlims=(0, 1), ylims=(0, 1),
+    plot(xlabel="x",
+        ylabel="y",
+        xlims=(0, 1),
+        ylims=(0, 1),
         legend=false,
         framestyle=:box,
         grid=:true,
@@ -25,47 +27,60 @@ function doplot(dots, updown, f, x, y, data, X, q, R)
     if dots
         X = data[:, 1]
         Y = data[:, 2]
-        display(plot!(X, Y,
-            seriestype=:line, color=:black, markershape=:circle, markersize=3))
+        plot!(X, Y,
+            seriestype=:line,
+            color=:black,
+            markershape=:circle,
+            markersize=3)
     else
         X = collect(range(0, 1, length=101))
         Y = f.(X)
-        display(plot!(X, Y,
-            seriestype=:line, color=:black))
+        plot!(X, Y,
+            seriestype=:line,
+            color=:black)
     end
     X = [0; 1]
     Y = X
-    display(plot!(X, Y,
-        seriestype=:line, color=:black,
-        linestyle=:dash))
+    plot!(X, Y,
+        seriestype=:line,
+        color=:black,
+        linestyle=:dash)
     Y = R / (1 + R) .* X .+ xD / (1 + R)
-    display(plot!(X, Y,
-        seriestype=:line, color=:blue))
-    display(plot!([xD],
-        seriestype=:vline, color=:blue,
-        linestyle=:dash))
+    plot!(X, Y,
+        seriestype=:line,
+        color=:blue)
+    plot!([xD],
+        seriestype=:vline,
+        color=:blue,
+        linestyle=:dash)
     Y = (xB - yi) / (xB - xi) .* (X .- xi) .+ yi
-    display(plot!(X, Y,
-        seriestype=:line, color=:red))
-    display(plot!([xB],
-        seriestype=:vline, color=:red,
-        linestyle=:dash))
+    plot!(X, Y,
+        seriestype=:line,
+        color=:red)
+    plot!([xB],
+        seriestype=:vline,
+        color=:red,
+        linestyle=:dash)
     if q != 1 - 1e-10
         Y = q / (q - 1) .* X .- xF / (q - 1)
-        display(plot!(X, Y,
-            seriestype=:line, color=:magenta,
-            linestyle=:dash))
+        plot!(X, Y,
+            seriestype=:line,
+            color=:magenta,
+            linestyle=:dash)
     end
-    display(plot!([xF],
-        seriestype=:vline, color=:magenta,
-        linestyle=:dash))
+    plot!([xF],
+        seriestype=:vline,
+        color=:magenta,
+        linestyle=:dash)
     if updown
         display(plot!(x, y,
-            seriestype=:steppost, color=:cyan,
+            seriestype=:steppost,
+            color=:cyan,
             linestyle=:solid))
     else
         display(plot!(x, y,
-            seriestype=:steppre, color=:cyan,
+            seriestype=:steppre,
+            color=:cyan,
             linestyle=:solid))
     end
 end
