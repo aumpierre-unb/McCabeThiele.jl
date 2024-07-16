cbb15975c73aa16c096830bb731e971eff27903f
Alexandre Umpierre <aumpierre@gmail.com>
2024-07-16 06:24:13 -0300

    update

--- src/McCabeThiele.jl
+++ src/McCabeThiele.jl
@@ -7,7 +7,7 @@ Author: Alexandre Umpierre `aumpierre@gmail.com`
 
 Maintainer's repository: `https://github.com/aumpierre-unb/McCabe-Thiele.jl`
 
-Citation (any version): `DOI 10.5281/zenodo.7126164`
+Citation (all versions): `DOI 10.5281/zenodo.7126164`
 
 See also: `stages`, `refmin`, `qR2S`, `qS2R`, `RS2q`.
 """
